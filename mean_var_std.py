cb097136ee2d2090474c688c8c743bfc70de354c
Keith Alexander Jr <99139089+kalexander914@users.noreply.github.com>
2022-04-10 11:09:20 -0500

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -2,46 +2,46 @@ import numpy as np
 
 def calculate(list):
 
-#condition of list lenght
+#Conditional Statement of length list smaller than 9
   if len(list) != 9:
     raise ValueError("List must contain nine numbers.")
 
-#create matrix
+#create 3x3 matrix
   else:
-    array = np.array(list).reshape((3, 3))
+    array_list = np.array(list).reshape((3, 3))
 
-#create dictionary
+#create glossary for arithmetic operations
 
-  calculations = {
+  arithmetic = {
     'mean': [
-      np.mean(array, axis = 0).tolist(),
-      np.mean(array, axis = 1).tolist(),
-      np.mean(array).tolist()],
+      np.mean(array_list, axis = 0).tolist(),
+      np.mean(array_list, axis = 1).tolist(),
+      np.mean(array_list).tolist()],
 
     'variance': [
-      np.var(array, axis = 0).tolist(),
-      np.var(array, axis = 1).tolist(),
-      np.var(array).tolist()],  
+      np.var(array_list, axis = 0).tolist(),
+      np.var(array_list, axis = 1).tolist(),
+      np.var(array_list).tolist()],
 
     'standard deviation': [
-      np.std(array, axis = 0).tolist(),
-      np.std(array, axis = 1).tolist(),
-      np.std(array).tolist()],
+      np.std(array_list, axis = 0).tolist(),
+      np.std(array_list, axis = 1).tolist(),
+      np.std(array_list).tolist()],
 
     'max': [
-      np.max(array, axis = 0).tolist(),
-      np.max(array, axis = 1).tolist(),
-      np.max(array).tolist()],
+      np.max(array_list, axis = 0).tolist(),
+      np.max(array_list, axis = 1).tolist(),
+      np.max(array_list).tolist()],
 
     'min': [
-      np.min(array, axis = 0).tolist(),
-      np.min(array, axis = 1).tolist(),
-      np.min(array).tolist()],  
+      np.min(array_list, axis = 0).tolist(),
+      np.min(array_list, axis = 1).tolist(),
+      np.min(array_list).tolist()],
 
     'sum': [
-      np.sum(array, axis = 0).tolist(),
-      np.sum(array, axis = 1).tolist(),
-      np.sum(array).tolist()]
+      np.sum(array_list, axis = 0).tolist(),
+      np.sum(array_list, axis = 1).tolist(),
+      np.sum(array_list).tolist()]
   }
 
-  return calculations
+  return arithmetic
